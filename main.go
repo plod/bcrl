ee08c0bb49fcd2308985044069476d92b20037d8
Daniel T. Morgan <dtmorgan@fields-group.com>
2017-02-12 22:35:13 +0000

    Adding fail

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
 	"runtime"
 	"strconv"
 )
 
 func main() {
 	version, err := strconv.ParseFloat(runtime.Version()[2:])
+	if err != nil {
+		log.Fatalln("Couln't reliably find golang version [comment this code block out if you are using more than go1.8]", err)
+	}
 }
