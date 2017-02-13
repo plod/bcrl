fbd75f183a8a6860ee059aa8c41b0867b82f1b0e
Daniel T. Morgan <dtmorgan@fields-group.com>
2017-02-13 23:36:14 +0000

    Adding precision for all cpu types

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	version, err := strconv.ParseFloat(runtime.Version()[2:])
+	version, err := strconv.ParseFloat(runtime.Version()[2:], 32)
 	if err != nil {
 		log.Fatalln("Couln't reliably find golang version [comment this code block out if you are using more than go1.8]", err)
 	}
+	log.Println(version)
 }
