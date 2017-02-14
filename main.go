c1edcaec6b5bb3550c62ecd594758c73357b607c
Daniel T. Morgan <dtmorgan@fields-group.com>
2017-02-14 00:37:15 +0000

    Changing to decimel (should work until this no longer becomes issue)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	version, err := strconv.ParseFloat(runtime.Version()[2:], 32)
+	version, err := strconv.Atoi(runtime.Version()[4:])
 	if err != nil {
 		log.Fatalln("Couln't reliably find golang version [comment this code block out if you are using more than go1.8]", err)
 	}
