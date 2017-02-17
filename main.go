4fa7fe9f5d174452787a22f9e76dbf8a36ad8f36
Daniel T. Morgan <dtmorgan@fields-group.com>
2017-02-17 03:40:18 +0000

    Using Correct date

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func init() {
 	if err != nil {
 		log.Fatalln("Couln't reliably find golang version [comment this code block out if you are using more than go1.8]", err)
 	}
-	if version < 9 {
+	if version < 8 {
 		log.Fatalln("Minimum version go1.8 required for this project")
 	}
 }
 
 func main() {
-
+	log.Println("Great you have a good enough version")
 }
